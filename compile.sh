38467daeef7c5042f35dd7f5ed3ac722f05198ef
Eric Floehr <eric@intellovations.com>
2025-01-01 20:57:02 -0500

    First commit

---
+++ compile.sh
@@ -0,0 +1,4 @@
+arm-none-eabi-as -g -a=hellorld.list hellorld.s -o hellorld.o
+arm-none-eabi-ld -g hellorld.o -T./stm32f103rb.ld -o hellorld.elf
+arm-none-eabi-objcopy -O binary hellorld.elf hellorld.bin
+st-flash write hellorld.bin 0x8000000
